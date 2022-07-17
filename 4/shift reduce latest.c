2038c8047248044870f49d8850cdb411e2956a70
Thiloth b shetty <93128235+thiloth@users.noreply.github.com>
2022-07-17 18:42:19 +0530

    Update shift reduce latest.c

--- 4/shift reduce latest.c
+++ 4/shift reduce latest.c
@@ -6,94 +6,94 @@ char a[20],ac[20],stk[20],act[20];
 void check();
 void main()
 {
- puts("GRAMMAR is E->E+T|T \n T->T*F|F \n F->(E) \n F->ID");
- puts("Enter input string");
- gets(a);
- //scanf("%s",&a);
- c=strlen(a);
- strcpy(act,"SHIFT->");
- puts("stack \t input \t action");
- for(i=0;j<c;i++,j++)
- {
-  if(a[j]=='i' && a[j+1]=='d')
-  {
-   stk[i]=a[j];
-   stk[i+1]=a[j+1];
-   a[j]=' ';
-   a[j+1]=' ';
-   printf("\n$%s\t%s$\t%sid",stk,a,act);
-   check();
-  }
-  else
-  {
-   stk[i]=a[j];
-   a[j]=' ';
-   printf("\n$%s\t%s$\t%ssymbols",stk,a,act);
-   check();
-  }
- }
-}
-//--------------------------------------------------------------------------------------------
-void check()
-{
- strcpy(ac,"REDUCE");
- //first for
- for(z=0;z<c;z++)
- {
-  if(stk[z]=='(' && stk[z+1]=='E' && stk[z+2]==')')
-  {
-   stk[z]='F';
-   stk[z+1]='\0';
-   stk[z+2]='\0';
-   printf("\n$%s\t%s$\t%s",stk,a,ac);
-   i=i-2;
-  }
-  else if(stk[z]=='i' && stk[z+1]=='d')
-  {
-   stk[z]='F';
-   stk[z+1]='\0';
-   printf("\n$%s\t%s$\t%s",stk,a,ac);
-   j++;
-  }
- }
-//2nd for
- for(z=0;z<c;z++)
- {
-  if(stk[z]=='T' && stk[z+1]=='*' && stk[z+2]=='F')
-  {
-   stk[z]='T';
-   stk[z+1]='\0';
-   stk[z+2]='\0';
-   printf("\n$%s\t%s$\t%s",stk,a,ac);
-   i=i-2;
-  }
-  else if(stk[z]=='F')
-  {
-   stk[z]='T';
-   printf("\n$%s\t%s$\t%s",stk,a,ac);
-  }
- }
-//3rd for
- for(z=0;z<c;z++)
- {
-  if(stk[z]=='E' && stk[z+1]=='+' && stk[z+2]=='T' && stk[z+3]=='*')break;
-  if(stk[z]=='E' && stk[z+1]=='+' && stk[z+2]=='T')
-  {
-   if(a[j+1]=='*')
-    break;
-   else
+   puts("GRAMMAR is E->E+T|T \n T->T*F|F \n F->(E) \n F->ID");
+   puts("Enter input string");
+   gets(a);
+   //scanf("%s",&a);
+   c=strlen(a);
+   strcpy(act,"SHIFT->");
+   puts("stack \t input \t action");
+   for(i=0;j<c;i++,j++)
    {
-    stk[z]='E';
-    stk[z+1]='\0';
-    stk[z+2]='\0';
-    printf("\n$%s\t%s$\t%s",stk,a,ac);
-    i=i-2;
+    if(a[j]=='i' && a[j+1]=='d')
+    {
+     stk[i]=a[j];
+     stk[i+1]=a[j+1];
+     a[j]=' ';
+     a[j+1]=' ';
+     printf("\n$%s\t%s$\t%sid",stk,a,act);
+     check();
+    }
+    else
+    {
+     stk[i]=a[j];
+     a[j]=' ';
+     printf("\n$%s\t%s$\t%ssymbols",stk,a,act);
+     check();
+    }
    }
   }
-  else if(stk[z]=='T')
+//--------------------------------------------------------------------------------------------
+  void check()
   {
-   stk[z]='E';
-   printf("\n$%s\t%s$\t%s",stk,a,ac);
-  }
- }//for ends
+   strcpy(ac,"REDUCE");
+    //first for
+   for(z=0;z<c;z++)
+   {
+    if(stk[z]=='(' && stk[z+1]=='E' && stk[z+2]==')')
+    {
+     stk[z]='F';
+     stk[z+1]='\0';
+     stk[z+2]='\0';
+     printf("\n$%s\t%s$\t%s",stk,a,ac);
+     i=i-2;
+    }
+    else if(stk[z]=='i' && stk[z+1]=='d')
+    {
+     stk[z]='F';
+     stk[z+1]='\0';
+     printf("\n$%s\t%s$\t%s",stk,a,ac);
+     j++;
+    }
+   }
+    //2nd for
+   for(z=0;z<c;z++)
+   {
+    if(stk[z]=='T' && stk[z+1]=='*' && stk[z+2]=='F')
+    {
+     stk[z]='T';
+     stk[z+1]='\0';
+     stk[z+2]='\0';
+     printf("\n$%s\t%s$\t%s",stk,a,ac);
+     i=i-2;
+    }
+    else if(stk[z]=='F')
+    {
+     stk[z]='T';
+     printf("\n$%s\t%s$\t%s",stk,a,ac);
+    }
+   }
+  //3rd for
+   for(z=0;z<c;z++)
+   {
+    if(stk[z]=='E' && stk[z+1]=='+' && stk[z+2]=='T' && stk[z+3]=='*')break;
+    if(stk[z]=='E' && stk[z+1]=='+' && stk[z+2]=='T')
+    {
+     if(a[j+1]=='*')
+      break;
+     else
+     {
+      stk[z]='E';
+      stk[z+1]='\0';
+      stk[z+2]='\0';
+      printf("\n$%s\t%s$\t%s",stk,a,ac);
+      i=i-2;
+     }
+    }
+    else if(stk[z]=='T')
+    {
+     stk[z]='E';
+     printf("\n$%s\t%s$\t%s",stk,a,ac);
+    }
+  }//for ends
 }//check()
